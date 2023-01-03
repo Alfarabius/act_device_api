2bfc9979f9ebaeff99951404d8c021bb6ace16b9
Turbo Laser <galiakberovigor@gmail.com>
2023-01-03 15:35:13 +0300

    work on flask sql client

--- sql_client/flask_app_client.py
+++ sql_client/flask_app_client.py
@@ -1,7 +1,6 @@
+from config import DSN
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-from config import DSN
 
 
 class SQLDeviceAPP:
@@ -15,4 +14,5 @@ sql_device_app = SQLDeviceAPP(DSN)
 
 
 if __name__ == "__main__":
+    sql_device_app.db.create_all()
     sql_device_app.app.run(debug=True)
